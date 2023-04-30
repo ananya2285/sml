7a8e91c6870d72bace494b8e945d671af9d406b4
ananya2285 <111894209+ananya2285@users.noreply.github.com>
2023-04-30 21:12:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import pandas as pd
 import numpy as np
 
 # Load the saved model
-with open('model_rfc.pkl', 'wb') as file:
+with open("C:\Users\kumar\dep\model_rfc.pkl", 'rb') as file:
     model = joblib.load(file)
+
 
 # Create the Streamlit app
 def app():
@@ -15,31 +16,37 @@ def app():
     st.title('Fraud Detection App')
     st.subheader('Detect fraudulent transactions')
 
-
     # Create input fields for user to enter data
     st.subheader('Enter transaction details:')
     step = st.number_input('Step', min_value=1)
     amount = st.number_input('Amount', min_value=0.0)
     oldbalanceOrg = st.number_input('Old Balance Org', min_value=0.0)
     newbalanceOrig = st.number_input('New Balance Orig', min_value=0.0)
+    oldbalanceDest = st.number_input('Old Balance Dest', min_value=0.0)
+    newbalanceDest = st.number_input('New Balance Dest', min_value=0.0)
+    isFlaggedFraud = st.number_input('Is Flagged Fraud', min_value=0)
     type = st.selectbox('Type of payment', ['CASH_OUT', 'TRANSFER', 'DEBIT', 'PAYMENT'])
-    # When the user clicks the 'Predict' button, make a prediction
-    input_array=[]
+
+    # Create input array based on user input
+    input_array = []
     input_array.append(step)
     input_array.append(amount)
     input_array.append(oldbalanceOrg)
     input_array.append(newbalanceOrig)
-    if(type=='CASH_OUT'):
+    input_array.append(oldbalanceDest)
+    input_array.append(newbalanceDest)
+    input_array.append(isFlaggedFraud)
+    if (type == 'CASH_OUT'):
         input_array.append(1)
         input_array.append(0)
         input_array.append(0)
         input_array.append(0)
-    elif(type=='TRANSFER'):
+    elif (type == 'TRANSFER'):
         input_array.append(0)
         input_array.append(1)
         input_array.append(0)
         input_array.append(0)
-    elif(type=='DEBIT'):
+    elif (type == 'DEBIT'):
         input_array.append(0)
         input_array.append(0)
         input_array.append(1)
@@ -49,20 +56,21 @@ def app():
         input_array.append(0)
         input_array.append(0)
         input_array.append(1)
-        input_array=np.array(input_array)
-        
-         # When the user clicks the 'Predict' button, make a prediction
+
+    # When the user clicks the 'Predict' button, make a prediction
     if st.button('Predict'):
         # Preprocess the user input
-        #input_array = preprocess_input(step, amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest, isFlaggedFraud, type_PAYMENT)
+        # input_array = preprocess_input(step, amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest, isFlaggedFraud, type_PAYMENT)
 
         # Make a prediction
-            prediction = model.predict([input_array])[0]
+        prediction = model.predict([input_array])[0]
 
         # Display the prediction to the user
-            if prediction == 0:
-                st.success('This transaction is not fraudulent.')
-            else:
-                st.error('This transaction is fraudulent.')
+        if prediction == 0:
+            st.success('This transaction is not fraudulent.')
+        else:
+            st.error('This transaction is fraudulent.')
+
 
 app()
+
