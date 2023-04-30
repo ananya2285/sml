6023274d5502c17933ae9a3509fbad94761976ea
ananya2285 <111894209+ananya2285@users.noreply.github.com>
2023-04-30 20:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 # Load the saved model
-with open('model_rfc.pkl', 'rb') as file:
+with open('model_rfc.pkl', 'wb') as file:
     model = joblib.load(file)
 
 # Create the Streamlit app
